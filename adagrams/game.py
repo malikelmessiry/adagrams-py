fabcf22ec95daaa1cf364e5665e7cafe54638fb5
Malik Elmessiry <malik.messiry@gmail.com>
2025-03-13 23:39:00 +0200

    first function returns a list of 10 numbers

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,7 +1,47 @@
-from random import randint
+from random import randint 
+
+#create letter pool, check to see if this C/V is ok 
+LETTER_POOL = {
+    'A': 9, 
+    'B': 2, 
+    'C': 2, 
+    'D': 4, 
+    'E': 12, 
+    'F': 2, 
+    'G': 3, 
+    'H': 2, 
+    'I': 9, 
+    'J': 1, 
+    'K': 1, 
+    'L': 4, 
+    'M': 2, 
+    'N': 6, 
+    'O': 8, 
+    'P': 2, 
+    'Q': 1, 
+    'R': 6, 
+    'S': 4, 
+    'T': 6, 
+    'U': 4, 
+    'V': 2, 
+    'W': 2, 
+    'X': 1, 
+    'Y': 2, 
+    'Z': 1
+}
 
 def draw_letters():
-    pass
+    hand = []
+    for i in range(10):
+        random_num = randint(1, 10)
+        hand.append(random_num)
+    print(hand)
+    return hand
+    # for letter, frequency in LETTER_POOL.items():
+draw_letters()
+
+
+
 
 def uses_available_letters(word, letter_bank):
     pass
